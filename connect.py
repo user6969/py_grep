9bb99d619972afc002d114552466c1d8693acc35
timur <tkhaziev@ninthdecimal.com>
2018-02-20 11:01:20 +0300

    Add missing functionality

---
+++ connect.py
@@ -0,0 +1,40 @@
+import paramiko
+
+
+USERNAME = 'username'
+PASSWORD = 'secret'
+
+
+def get_ip_address():
+    ip = raw_input('Enter server ip address: ')
+    return str(ip)
+
+
+def get_log_name():
+    name = raw_input('Enter log name mask: ')
+    return str(name)
+
+
+def get_identity():
+    number = raw_input('Enter identification number: ')
+    return number
+
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(get_ip_address(), username=USERNAME, password=PASSWORD)
+
+
+sftp = client.open_sftp()
+sftp.put(__file__, '/tmp/py_grep.py')
+sftp.close()
+
+log_name = get_log_name()
+identity = get_identity()
+stdout = client.exec_command('python /tmp/py_grep.py -{} -{}'.format(log_name, identity))[1]
+
+for line in stdout:
+    print line
+client.close()
+
+
